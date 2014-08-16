f87bed2267c4276c3d5303ce30c00f5259f677ce
Marcus Ekwall <marcus.ekwall@gmail.com>
2014-08-16 23:04:17 +0200

    Updated test

--- test/test.js
+++ test/test.js
@@ -25,44 +25,49 @@ app.use(function* (next) {
     }
 });
 
+var socket;
+
 describe('koa-ws', function () {
 
-    it ('should be able to attach middleware to app', function () {
+    it('expect to be able to attach middleware to app', function () {
         var middleware = require('../src/middleware');
         app.use(middleware(app));
         expect(app).to.have.property('ws');
     });
 
-    it ('should be able to register a simple route', function () {
+    it('expect to be able to register a simple server method', function () {
         app.ws.register('hello', function* () {
             this.result('world');
         });
-        expect(app.ws.methods.hello).to.be.a('function');
+        expect(app.ws._methods.hello).to.be.a('function');
     });
 
-    it ('should be able to register namespaced routes', function () {
+    it('expect to be able to register namespaced server methods', function () {
         app.ws.register('user', {
             create: function* () { this.result('ok'); },
             update: function* () { this.result('ok'); },
             delete: function* () { this.result('ok'); }
         });
 
-        expect(app.ws.methods['user:create']).to.be.a('function');
-        expect(app.ws.methods['user:update']).to.be.a('function');
-        expect(app.ws.methods['user:update']).to.be.a('function');
+        expect(app.ws._methods['user:create']).to.be.a('function');
+        expect(app.ws._methods['user:update']).to.be.a('function');
+        expect(app.ws._methods['user:update']).to.be.a('function');
     });
 
-    it ('should be able to listen to the same port as koa server', function (done) {
+    it('expect server to be able to listen to the same port as koa server', function (done) {
         app.listen(3000, function () {
             expect(app.ws.server._server).to.be.a('object');
             done();
         });
-
-        request(app.server)
-            .get('/');
+        // Connect and generate session
+        request(app.server).get('/');
     });
 
-    it ('websocket client should be able to connect to server', function (done) {
+    it('expect websocket client to be able to connect to server', function (done) {
+        app.ws.server.on('connection', function (wss) {
+            socket = wss;
+        });
+
         client = require('../src/client');
         client.connect();
         client.on('open', function () {
@@ -70,7 +75,7 @@ describe('koa-ws', function () {
         });
     });
 
-    it ('should get world when hello method is called', function (done) {
+    it('expect client to recieve result world when hello server method is called', function (done) {
         client.method('hello', function (err, payload) {
             expect(err).to.be.a('null');
             expect(payload).to.equal('world');
@@ -78,22 +83,55 @@ describe('koa-ws', function () {
         });
     });
 
-    it ('should be able to call namespaced events', function (done) {
+    it('excpect client to be able to call namespaced server methods', function (done) {
         client.method('user:create', function (err, payload) {
             expect(err).to.be.a('null');
+            expect(payload).to.equal('ok');
         });
 
         client.method('user:update', function (err, payload) {
             expect(err).to.be.a('null');
+            expect(payload).to.equal('ok');
         });
 
         client.method('user:delete', function (err, payload) {
             expect(err).to.be.a('null');
+            expect(payload).to.equal('ok');
             done();
         });
     });
 
-    it ('should return error -32700 parse error', function (done) {
+    it('expect to be able to register namespaced client methods', function () {
+        client.register('user', {
+            create: function () { this.result('ok'); },
+            update: function () { this.result('ok'); },
+            delete: function () { this.result('ok'); }
+        });
+
+        expect(client._methods['user:create']).to.be.a('function');
+        expect(client._methods['user:update']).to.be.a('function');
+        expect(client._methods['user:update']).to.be.a('function');
+    });
+
+    it('expect server to be able to call namespaced client methods', function (done) {
+        socket.method('user:create', function (err, payload) {
+            expect(err).to.be.a('null');
+            expect(payload).to.equal('ok');
+        });
+
+        socket.method('user:update', function (err, payload) {
+            expect(err).to.be.a('null');
+            expect(payload).to.equal('ok');
+        });
+
+        socket.method('user:delete', function (err, payload) {
+            expect(err).to.be.a('null');
+            expect(payload).to.equal('ok');
+            done();
+        });
+    });
+
+    it('expect server to return error -32700 parse error', function (done) {
         client.once('message', function (payload) {
             var payload = JSON.parse(payload);
             expect(payload.error.code).to.be.equal(-32700);
@@ -102,7 +140,7 @@ describe('koa-ws', function () {
         client.socket.send({ foo: 'bar' });
     });
 
-    it ('should return error -32600 invalid request', function (done) {
+    it('expect server to return error -32600 invalid request', function (done) {
         client.once('message', function (payload) {
             var payload = JSON.parse(payload);
             expect(payload.error.code).to.be.equal(-32600);
@@ -111,21 +149,21 @@ describe('koa-ws', function () {
         client.socket.send(JSON.stringify({ foo: 'bar' }));
     });
 
-    it ('should return error -32602 invalid params', function (done) {
+    it('expect server to return error -32602 invalid params', function (done) {
         client.method('hello', 'world!', function (err, payload) {
             expect(err.code).to.be.equal(-32602);
             done();
         });
     });
 
-    it ('should return error -32601 method not found', function (done) {
+    it('expect server to return error -32601 method not found', function (done) {
         client.method('foo', function (err, payload) {
             expect(err.code).to.be.equal(-32601);
             done();
         });
     });
 
-    it ('should serve the client library at /koaws.js', function (done) {
+    it('expect to be able to serve the client library at /koaws.js', function (done) {
         request(app.server)
             .get('/koaws.js')
             .expect(200)
